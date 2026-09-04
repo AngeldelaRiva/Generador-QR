65e2c133d23c750673d74b7415d49a4273019b91
AngeldelaRiva <urbinaa363@gmail.com>
2026-09-03 22:59:38 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'qr-gen-v1';
+const FILES = [
+  './index.html',
+  './manifest.json',
+  './qrcode.min.js',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
