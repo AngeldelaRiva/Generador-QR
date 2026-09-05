c50ba0c615ad1c6ab368c6b5d91d92beb5e99987
AngeldelaRiva <urbinaa363@gmail.com>
2026-09-04 23:12:53 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,50 @@
-const CACHE_NAME = 'qr-gen-v2';
-const FILES = ['./index.html', './manifest.json', './qrcode.min.js', './jsbarcode.min.js', './jsqr.min.js', './icon-192.png', './icon-512.png'];
+const CACHE_NAME = 'qr-gen-v3';
+
+const FILES = [
+  './',
+  './index.html',
+  './manifest.json',
+  './qrcode.min.js',
+  './jsbarcode.min.js',
+  './jsqr.min.js',
+  './icon-192.png',
+  './icon-512.png'
+];
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES))
+  );
+
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))))
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
   );
+
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(caches.match(event.request).then((r) => r || fetch(event.request)));
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, copy);
+        });
+
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
 });
