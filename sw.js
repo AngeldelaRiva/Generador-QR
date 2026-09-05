91ef0a3b91243db08159740ca18c46a6b0bac0a9
AngeldelaRiva <urbinaa363@gmail.com>
2026-09-04 21:44:22 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,16 @@
-const CACHE_NAME = 'qr-gen-v1';
-const FILES = [
-  './index.html',
-  './manifest.json',
-  './qrcode.min.js',
-  './icon-192.png',
-  './icon-512.png'
-];
+const CACHE_NAME = 'qr-gen-v2';
+const FILES = ['./index.html', './manifest.json', './qrcode.min.js', './jsbarcode.min.js', './jsqr.min.js', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES)));
+});
+
+self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES))
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))))
   );
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
-  );
+  event.respondWith(caches.match(event.request).then((r) => r || fetch(event.request)));
 });
